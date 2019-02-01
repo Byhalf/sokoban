b4902fab423ba8024d7c6210c5e486f04cfb47d8
Byhalf <gareth.lafay@gmail.com>
2019-02-01 10:45:50 +0100

    petite modification ajout class state

---
+++ src/modele/Case.java
@@ -0,0 +1,5 @@
+package modele;
+
+public enum Case {
+    FLOOR,WALL,GOAL
+}
--- src/utulities/LevelMaker.java
+++ src/utulities/LevelMaker.java
@@ -1,4 +1,5 @@
 package utulities;
+import modele.Case;
 import java.awt.*;
 import java.util.ArrayList;
 
@@ -22,45 +23,6 @@ public class LevelMaker {
 
     public String getLevelAsString(int n){
         return getLevelsList()[n-1];
-    }
-
-    public int[] getDim(int lvlNbr){
-        String lvl = getLevelAsString(lvlNbr);
-        int dim[] = new int[2];
-        int height = 0;
-        int width = 0;
-        int counter = 0;
-        for(char s:lvl.toCharArray()){
-            if(s!='\n'){
-                counter +=1;
-            }else{
-                if (counter>width)
-                        width = counter;
-                counter = 0;
-                height += 1;
-            }
-        }
-
-        dim[0]=width;
-        dim[1]=height;
-        return dim;
-
-    }
-
-    public char[][] makeGameGrid(int lvlNbr){
-        int x = getDim(lvlNbr)[0];
-        int y = getDim(lvlNbr)[1];
-        char grid[][] = new char[x][y];
-        String lvl = getLevelAsString(lvlNbr);
-        for(int i=0;i<y;i++){
-            for(int j=0;j<x;j++){
-                if (lvl.charAt(j)!='\n'){
-                    grid[i][j] = lvl.charAt(j);
-                }else break;
-            }
-        }
-
-        return grid;
     }
 
 }
