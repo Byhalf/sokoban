f4b26f28ffb4585b3059642f521665997b1b5ed4
Pierre-Louis Klaeyle <pierrelouis.klaeyle@gmail.com>
2019-02-05 16:41:47 +0100

    avancée sur les objets déplacables

--- src/modele/movables/Box.java
+++ src/modele/movables/Box.java
@@ -8,22 +8,28 @@ public class Box extends Movable{
     public int testx;
     public int testy;
 
-    public void deplacement(char d){
-        super.deplacement(d);
-    }
-
-    public boolean isPossible(State grid,int x,int y){
-        if (!super.isPossible(grid,x,y)){
-            return false;
-        }
-        return true;
-
-    }
-
     public Box(int x,int y){
         super(x,y);
 
     }
+
+    @Override
+    public void deplacement(char d){
+        super.deplacement(d);
+    }
+    @Override
+    public boolean isPossible(State grid,int x,int y){
+        if (!super.isPossible(grid,x,y)){
+            return false;
+        }
+        for (Movable m:grid.movables) {
+            if (m.x == this.testx && m.y == this.testy){
+                return false;
+            }
+        return true;
+
+    }
+
 
 
 }
