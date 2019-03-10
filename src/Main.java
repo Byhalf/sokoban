909ea428a32d19281c9d465d4e9c06c558d16db4
Byhalf <gareth.lafay@gmail.com>
2019-03-10 17:33:25 +0100

    "test d'un keylistener "

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,12 @@ import vue.*;
 public class Main {
 
     public static void main(String[] args) throws Exception{
-        String filePath1 = "levels/lvl.sxb";
+        String filePath1 = "src/levels/lvl.sxb";
         String levels = FileParser.parse(filePath1);
         Levels levels_list = new Levels(levels);
         LevelMaker state = new LevelMaker(levels_list,1);
         State etat = state.setLevel(1);
+
 
     }
 }
