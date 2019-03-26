e4e14cfa8de975dd5117ee2a75d94a34c25a944a
Mialon Laurine <21705826@C304L-409C21.campus.unicaen.fr>
2019-03-26 17:50:47 +0100

    deux boutons ajoutés

--- src/vue/Gui.java
+++ src/vue/Gui.java
@@ -1,10 +1,10 @@
 package vue;
 
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import modele.State;
 import modele.movables.Direction;
 import modele.Modele;
 import javax.swing.*;
+import java.awt.*;
 import javax.swing.border.Border;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
@@ -12,17 +12,35 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Gui extends JFrame implements KeyListener {
+public class Gui extends JFrame implements KeyListener, ActionListener {
     Modele modele;
+    JButton buttonCancel, buttonRestart;
     SokobanVue sokovue;
+    JPanel pan;
     public Gui(Modele modele){
         super("sokoban");
         this.modele = modele;
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.addKeyListener(this);
+
         setLayout(new BorderLayout());
         sokovue = new SokobanVue(modele);
+	
+	pan = new JPanel();
+	pan.setLayout(new GridLayout(3,1));
+
+	buttonCancel = new JButton("Cancel");
+	buttonCancel.addActionListener(this);
+	pan.add(buttonCancel);
+	
+	buttonRestart = new JButton("Restart");
+	buttonRestart.addActionListener(this);
+	pan.add(buttonRestart);
+	
+	sokovue.add(pan);
         add(sokovue,BorderLayout.CENTER);
+	add(pan, BorderLayout.EAST);
+	
         pack();
         setVisible(true);
         setFocusable(true); //pour le keylistener
@@ -46,5 +64,18 @@ public class Gui extends JFrame implements KeyListener {
         }else if (e.getKeyCode() == KeyEvent.VK_DOWN){
             modele.deplacement(Direction.D);
         }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+	Object obj = e.getSource();
+	if (obj == buttonCancel) {
+		modele.annulDeplacement();
+		this.requestFocus();
+	}
+	else if (obj == buttonRestart) {
+		modele.resetLevel();
+		this.requestFocus();
+	}
     }
 }
